7694943069f29e147707229b72fc4fd5ba349701
Ellen <lnellen.w@gmail.com>
2016-01-20 15:04:25 -0500

    Make task priority an enum

--- include/task.h
+++ include/task.h
@@ -5,12 +5,19 @@
 #define T_ACTIVE 1
 #define T_ZOMBIE 2
 
+typedef enum {
+    LOW = 0,
+    MED,
+    HIGH,
+    TOP,
+} PRIORITY;
+
 struct Task {
     int tid, pid;
     int sp, lr, spsr;
     int ret;
     int state;
-    int priority;
+    PRIORITY priority;
 };
 
 #endif
