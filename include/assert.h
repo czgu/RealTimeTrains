ce5ae5804848e98a4a0d0a1a8390ccb120d8e3c7
Cheng Zhi Gu <czgu@uwaterloo.ca>
2016-03-20 21:25:27 -0400

    Fixed assert

--- include/assert.h
+++ include/assert.h
@@ -2,11 +2,12 @@
 #define _ASSERT_H_
 
 #include <bwio.h>
+#include <syscall.h>
 #include <ts7200.h>
 
 #ifdef _ASSERT
 #define ASSERT(expr)    \
-if (!expr) {\
+if (!(expr)) {\
     bwprintf(COM2, "assertion fail: %s, line %d\n\r", __FILE__, __LINE__);\
     Halt();\
 }
